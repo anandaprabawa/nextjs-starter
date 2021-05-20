4d1fee0a570b6fd9374c120a25cb4912b8abc6ff
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2021-05-20 18:47:37 +0800

    chore: setup test

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  moduleNameMapper: {
+    '\\.(css|scss|sass)$': 'identity-obj-proxy',
+    '\\.(svg|jpg|jpeg|png|webp)$': '<rootDir>/test/__mocks__/file-mock.js',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/\\.next/'],
+}
