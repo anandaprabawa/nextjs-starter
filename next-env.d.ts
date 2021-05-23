df29edf852a780bfd7d4b8e73766dd71b0ffe0dc
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2021-05-23 10:29:23 +0800

    feat: persistent layout

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,15 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+type PageAdditionalProps = {
+  layout?: import('react').ComponentType
+}
+
+declare type AppProps =
+  import('next/dist/next-server/lib/router/router').AppProps & {
+    Component: React.ComponentType & PageAdditionalProps
+  }
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+declare type NextPage<P = {}, IP = P> = import('next').NextPage<P, IP> &
+  PageAdditionalProps
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router'
+import React from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  const Layout = Component.layout || React.Fragment
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
